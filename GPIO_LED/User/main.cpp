7f2ff6169c9c26e0ddfcfe7bbce99009eb48fb4a
feifeigd <feifeigd@21cn.com>
2025-12-06 02:04:31 +0800

    利用 定时器2 翻转 PORTC.13

--- GPIO_LED/User/main.cpp
+++ GPIO_LED/User/main.cpp
@@ -94,9 +94,40 @@ public:
 #include <cstdio> // printf
 //#include <iostream>
 #include <vector>
-int main(void){
+
+void Timer_Config(){
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	TIM_InternalClockConfig(TIM2);
+	TIM_DeInit(TIM2);
+	// Period / (72M / (Prescaler + 1)) = _ 秒
+	// 1000 / (72000000 / 36000) = 0.5 秒
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure{
+		.TIM_Prescaler = 36000 - 1, // 预分频数
+		.TIM_CounterMode = TIM_CounterMode_Up, // 定时器计数为向上计数模式
+		.TIM_Period = 1000, // 定时器周期
+		.TIM_ClockDivision = TIM_CKD_DIV1, // TIM2 时钟分频，1表示不分频
+	};
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+	TIM_ClearFlag(TIM2, TIM_FLAG_Update); // 清除定时器2的溢出标志位
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // 使能定时器2溢出中断
+	TIM_Cmd(TIM2, ENABLE); // 定时器使能
+	
+	// 配置NVIC（以分组2为例）
+NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+NVIC_InitTypeDef NVIC_InitStructure;
+NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+NVIC_Init(&NVIC_InitStructure);
+	
+}
+
 	Application app;
+int main(void){
 	app.init();
+	Timer_Config();
 		
 	Usart::init(9600);
 	auto v = new std::vector<int>{00};
@@ -107,10 +138,25 @@ int main(void){
 	
 	while(1){
 		// 闪烁LED，指示系统运行
-		app.led.Toggle();
+		//app.led.Toggle();
 		Delay::ms(500);
 		Usart::update();		
 	}
 
 	return 0;
 }
+
+extern "C" void TIM2_IRQHandler(){
+	static bool flag_bit = false;
+	
+	if(TIM_GetITStatus(TIM2, TIM_IT_Update) == SET){
+		TIM_ClearITPendingBit(TIM2, TIM_FLAG_Update); // 清除溢出中断标志位
+		flag_bit = !flag_bit;
+		if(flag_bit){
+			app.led.Off();
+		}else{
+			app.led.On();
+		}		
+	}
+}
+
